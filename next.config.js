abbdab8cb0d99b9ad9bb949eb20caf483eecf4fd
@nikita.osypa <nikita.osypa@codifly.be>
2021-08-07 09:07:43 +0300

    Initial next set-up

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.fbx$/i,
+      use: 'raw-loader'
+    })
+    return config
+  }
 }
