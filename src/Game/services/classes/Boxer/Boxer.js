69456f506ccc314ccdfeb4dc668bc6010f245af1
maldenol <mineden2300mo@gmail.com>
2021-08-22 12:03:35 +0300

    added animation request and immediate request methods

--- src/Game/services/classes/Boxer/Boxer.js
+++ src/Game/services/classes/Boxer/Boxer.js
@@ -1,3 +1,5 @@
+import { LoopRepeat } from 'three';
+
 import boxerParameters from '../../constants/boxerParameters';
 
 
@@ -7,20 +9,25 @@ class Boxer {
     this.animationMixer = animationMixer;
     this.animationActions = animationActions;
     this.currentAnimationName = 'warming-up';
+    this.animationQueue = [];
   }
 
   animate(deltaTime) {
     this.animationMixer.update(deltaTime);
-    if (!this.animationActions[this.currentAnimationName].isRunning()) {
-      this.requestAnimation('fighting-idle');
+    if (this.animationActions[this.currentAnimationName].isRunning()) {
+      if (this.animationActions[this.currentAnimationName].loop === LoopRepeat && this.animationQueue.length !== 0) {
+        this.requestImmediateAnimation(this.animationQueue.shift());
+      }
+    } else {
+      this.requestImmediateAnimation(this.animationQueue.length === 0 ? 'fighting-idle' : this.animationQueue.shift());
     }
   }
 
-  requestAnimation(name) {
+  requestImmediateAnimation(name) {
     if (name in this.animationActions) {
       if (name !== this.currentAnimationName) {
         this.animationActions[name].reset();
-        this.animationActions[name].crossFadeFrom(this.animationActions[this.currentAnimationName], 0.5);
+        this.animationActions[name].crossFadeFrom(this.animationActions[this.currentAnimationName], 0.1);
         this.animationActions[name].play();
         this.currentAnimationName = name;
       } else if (!this.animationActions[this.currentAnimationName].isRunning()) {
@@ -28,6 +35,14 @@ class Boxer {
         this.animationActions[name].play();
       }
     }
+  }
+
+  requestAnimation(name) {
+    this.animationQueue.push(name);
+  }
+
+  emptyAnimationQueue() {
+    this.animationQueue = [];
   }
 }
 
