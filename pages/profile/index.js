a26830f4cb561d1f961deeb632d041c2ef124572
VladTkach <vladand2003@gmail.com>
2021-10-07 22:46:54 +0300

    test2

---
+++ pages/profile/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Market = (props) => {
+  return <div className="global__profile">
+    <div className="profile__header">
+      <div className="profile__container">
+        <div className="profile__container__inner">
+          <div className="profile__photo">
+        
+      
+            </div>
+          <div className="profile__text">
+            <div className="profile__info">
+              <div className="profile__name">
+                My Stable
+              </div>
+              <div className="profile__nick">
+                stable 1212112
+              </div>
+            </div>
+
+            <div className="profile__settings">
+              <div className="profile__settings_icon">
+                {/* <img src="" alt="" width="10" >  */}
+              </div>
+              <div className="profile__settings_text">
+                Settings
+              </div>
+            </div>
+
+            </div>
+      
+          </div>
+        </div>
+      
+    </div>
+    </div>;
+};
+
+export default Market;
