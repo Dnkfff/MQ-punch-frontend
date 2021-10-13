5e88292ffd71a329e926a59e53c78706de33fb0e
VladTkach <vladand2003@gmail.com>
2021-10-13 21:34:14 +0300

    market and social

--- pages/market/index.js
+++ pages/market/index.js
@@ -1,9 +1,92 @@
-import React from 'react';
+import React from "react";
 
 const Market = (props) => {
   return (
-    <div className='global-market-page'>
-      <p className='class'>123123123</p>
+    <div className="global-market-page">
+      <div className="market">
+        <div className="market__item">
+          <div className="puncher__photo"></div>
+          <div className="puncher__info">
+            <div className="puncher__name">
+              <div className="puncher__info__title">MQ-punch</div>
+              <div className="puncher__nick">Joe</div>
+            </div>
+            <div className="puncher__price">
+              <div className="puncher__info__title">Price</div>
+              <div className="puncher__price__">
+                <div className="puncher__price__icon"></div>
+                <div className="puncher__price__value">3</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="market__item">
+          <div className="puncher__photo"></div>
+          <div className="puncher__info">
+            <div className="puncher__name">
+              <div className="puncher__info__title">MQ-punch</div>
+              <div className="puncher__nick">Joe</div>
+            </div>
+            <div className="puncher__price">
+              <div className="puncher__info__title">Price</div>
+              <div className="puncher__price__">
+                <div className="puncher__price__icon"></div>
+                <div className="puncher__price__value">3</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="market__item">
+          <div className="puncher__photo"></div>
+          <div className="puncher__info">
+            <div className="puncher__name">
+              <div className="puncher__info__title">MQ-punch</div>
+              <div className="puncher__nick">Joe</div>
+            </div>
+            <div className="puncher__price">
+              <div className="puncher__info__title">Price</div>
+              <div className="puncher__price__">
+                <div className="puncher__price__icon"></div>
+                <div className="puncher__price__value">3</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="market__item">
+          <div className="puncher__photo"></div>
+          <div className="puncher__info">
+            <div className="puncher__name">
+              <div className="puncher__info__title">MQ-punch</div>
+              <div className="puncher__nick">Joe</div>
+            </div>
+            <div className="puncher__price">
+              <div className="puncher__info__title">Price</div>
+              <div className="puncher__price__">
+                <div className="puncher__price__icon"></div>
+                <div className="puncher__price__value">3</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="maklet__social">
+        <div className="market__container">
+          <div className="market__social__inner">
+            <div className="market__social__title">
+              Join our discord and telegram for trade
+            </div>
+            <div className="market__social__btn">
+              <button className="market__btn" type="button">
+                Discord
+              </button>
+              <button className="market__btn" type="button">
+                Telegram
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
