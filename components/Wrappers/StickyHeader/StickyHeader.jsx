f99b6d5a7fc7658e94189e20bc0267d48173c71d
17etro <petrpetryk2016@gmail.com>
2021-10-15 23:01:19 +0300

    fixed svgs

--- components/Wrappers/StickyHeader/StickyHeader.jsx
+++ components/Wrappers/StickyHeader/StickyHeader.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import Link from "next/link";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
 
 // components
-import BigHeaderMenuLinks from "../BigHeaderMenuLinks/BigHeaderMenuLinks";
+import BigHeaderMenuLinks from '../BigHeaderMenuLinks/BigHeaderMenuLinks';
 
 // functions
-import { changeMobileMenuOpened } from "../../../redux/reducers/globalManager/slice";
+import { changeMobileMenuOpened } from '../../../redux/reducers/globalManager/slice';
 
 // assets
-import logotype from "../../../assets/website/logos/mq-punch-logo-small.png";
-import creditCardSVG from "../../../assets/website/icons/credit_card.svg";
-import profileIconSVG from "../../../assets/website/icons/profile_icon.svg";
-import { HiMenuAlt2 } from "react-icons/hi";
+import logotype from '../../../assets/website/logos/mq-punch-logo-small.png';
+import CreditCardSVG from '../../../assets/website/icons/credit_card.svg';
+import ProfileIconSVG from '../../../assets/website/icons/profile_icon.svg';
+import { HiMenuAlt2 } from 'react-icons/hi';
 
 const StickyHeader = (props) => {
   const dispatch = useDispatch();
@@ -20,7 +20,7 @@ const StickyHeader = (props) => {
 
   const userIsAuth = !!useSelector((state) => state.auth.user);
   const smallHeaderClassName = `app-container-sticky-header ${
-    showStickyHeader ? "opened" : "closed"
+    showStickyHeader ? 'opened' : 'closed'
   }`;
 
   const onOpenMobileMenu = () => {
@@ -29,30 +29,30 @@ const StickyHeader = (props) => {
 
   return (
     <div className={smallHeaderClassName}>
-      <div className="left">
-        <Link href="/">
-          <div className="logotype">
-            <img src={logotype.src} alt="MQPUNCH" />
+      <div className='left'>
+        <Link href='/'>
+          <div className='logotype'>
+            <img src={logotype.src} alt='MQPUNCH' />
           </div>
         </Link>
 
-        <div className="menu-icon" onClick={onOpenMobileMenu}>
+        <div className='menu-icon' onClick={onOpenMobileMenu}>
           <HiMenuAlt2 />
         </div>
 
-        <nav className="sticky-header-links">
+        <nav className='sticky-header-links'>
           <BigHeaderMenuLinks />
         </nav>
       </div>
       {userIsAuth && (
-        <div className="right">
-          <div className="balance">
-            <img src={creditCardSVG.src} alt="credit_card" />
-            <span>{"300.00 $"}</span>
+        <div className='right'>
+          <div className='balance'>
+            <CreditCardSVG />
+            <span>{'300.00 $'}</span>
           </div>
-          <Link href={"/profile"}>
-            <div className="profile">
-              <img src={profileIconSVG.src} alt="profile" />
+          <Link href={'/profile'}>
+            <div className='profile'>
+              <ProfileIconSVG />
             </div>
           </Link>
         </div>
