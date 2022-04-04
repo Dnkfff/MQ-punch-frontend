7ddbe2f1d5d4635c291f15cbbaac9d1273a48105
ykovalenko <y.kovalenko@betsy.games>
2022-04-04 21:50:24 +0300

    learn page

--- pages/learn/index.js
+++ pages/learn/index.js
@@ -2,15 +2,11 @@ import React, { useEffect } from 'react';
 import Router from 'next/router';
 import { GUIDE_URL } from '../../inside-services/constants/constants';
 
-
-const redirect = () => {
-  Router.push('/', undefined, { shallow: true });
-  window.open(GUIDE_URL);
-};
 const Learn = () => {
 
   useEffect(() => {
-    redirect();
+    Router.push('/', undefined, { shallow: true });
+    window.open(GUIDE_URL).focus();
   }, []);
 
   return <></>;
